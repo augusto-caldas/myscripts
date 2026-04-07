b51d580e1e5c0d6473fb8d068c2a42716d7f5424
Luis Caldas <augusto@caldas.ie>
2026-04-08 00:09:37 +0100

    prompt reboot

--- startup-scripts/update-nixos.sh
+++ startup-scripts/update-nixos.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-sudo nix-channel --update
-sudo nixos-rebuild boot
+# Update and ebuild
+sudo nixos-rebuild boot --upgrade
 sudo nix-collect-garbage --delete-old
+
+# Prompt the user for a reboot
+read -p "Reboot now? (y/N): " reboot
+if [[ "${reboot,,}" == "y" || "${reboot,,}" == "yes" ]]; then
+    sudo reboot
+else
+    echo "Reboot skipped."
+fi
